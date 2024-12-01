ea2425961eafa32d0e3534163c0294752e12ac21
sonuswami0100 <sonusjag960@gmail.com>
2024-12-01 23:24:04 +0530

    nothing changes

--- c++ DSA/Array/tempCodeRunnerFile.cpp
+++ c++ DSA/Array/tempCodeRunnerFile.cpp
@@ -1,1 +1,10 @@
-swap(arr[minI],arr[maxI]);
+#include<iostream>
+using namespace std;
+void uniqueVal(int arr[] , int size){
+    for(int i = 0;i < size;i++){
+        bool isunique = true;
+        for(int j = 0;j < size;j++){
+            if(i != j && arr[i] == arr[j]){
+                isunique = false;
+                break;
+            }
